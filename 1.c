c962f8eee9f5aca036d3c5f4302136b74ae48a26
Zhao kai <601791946@qq.com>
2011-08-12 18:39:26 -0700

    change the value that p point to, and the variable is changed

--- 1.c
+++ 1.c
@@ -5,7 +5,10 @@ int main(int argc, char *argv[])
     int i;
     i = 1;
     int *p = &i;
-    printf("0x%x\n", p);
-    printf("0x%x\n", &i);
+    printf("the address is: 0x%x\n", p);
+    printf("the content is: %d\n", *p);
+    printf("the address is: 0x%x\n", &i);
+    *p = 2;
+    printf("the new content is: %d\n", i);
     return 0;
 }
